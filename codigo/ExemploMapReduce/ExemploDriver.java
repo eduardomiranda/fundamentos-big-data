a2e508db575abb971e9da84070c725edddf58cbb
Eduardo Miranda <eduardo@emiranda.com.br>
2016-04-02 16:02:27 -0300

    Exemplo base de uma tarefa MapReduce onde o objetivo é obter o menor o menor valor e o respectivo ano do conjunto de dados Summary of Receipts, Outlays, and Surpluses or Deficits (-): 1789–2014

---
+++ codigo/ExemploMapReduce/ExemploDriver.java
@@ -0,0 +1,49 @@
+package Exemplo; 
+
+import java.util.*;
+
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+
+
+public class ExemploDriver extends Configured implements Tool {
+
+   public int run(String[] args) throws Exception {
+      // Verifica a passagem do arquivo de dados e do diretório de saída
+      if (args.length != 2) {
+         System.err.printf("Argumentos exigidos pela classe %s: <ArquivoEntrada> <DiretorioSaida>\n", getClass().getSimpleName());
+         System.exit(1);
+      }
+
+      Job job = new Job(getConf(), "Exemplo de MapReduce");
+
+      job.setJarByClass(ExemploDriver.class);
+      job.setMapperClass(ExemploMapper.class);
+      job.setReducerClass(ExemploReducer.class);
+
+      job.setInputFormatClass(TextInputFormat.class);
+      job.setOutputKeyClass(Text.class);
+      job.setOutputValueClass(Text.class);
+
+      // Passa o arquivo de entrada e o diretório de saída
+      FileInputFormat.addInputPath(job, new Path(args[0]));
+      FileOutputFormat.setOutputPath(job, new Path(args[1])); 
+
+      // Executa a tarefa de forma síncrona
+      return  job.waitForCompletion(true) ? 0 : 1;
+   }
+
+   public static void main(String[] args) throws Exception { 
+      Configuration conf = new Configuration();
+      System.exit(ToolRunner.run(conf, new ExemploDriver(), args));
+   } 
+}
