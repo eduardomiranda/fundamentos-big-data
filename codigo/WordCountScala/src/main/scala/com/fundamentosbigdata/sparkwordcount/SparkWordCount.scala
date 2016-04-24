67ef1f69b165851d42bd3639c7d4a86fb33db650
Eduardo Miranda <eduardo@emiranda.com.br>
2016-04-24 13:27:13 -0300

    Adição de comentários na classe SparkWordCount

--- codigo/WordCountScala/src/main/scala/com/fundamentosbigdata/sparkwordcount/SparkWordCount.scala
+++ codigo/WordCountScala/src/main/scala/com/fundamentosbigdata/sparkwordcount/SparkWordCount.scala
@@ -6,11 +6,16 @@ import org.apache.spark.SparkConf
 
 object SparkWordCount {
   def main(args: Array[String]) {
-    val sc = new SparkContext(new SparkConf().setAppName("Spark Count"))
-    val centerEarthRDD = sc.textFile(args(0))
+val sc = new SparkContext(new SparkConf().setAppName("Spark Count"))
+
+val centerEarthRDD = sc.textFile(args(0))
+
+	// Separa as palavras do documento
 	val tokenizedRDD = centerEarthRDD.flatMap(linha => linha.split(" "))
+
+	// Conta a ocorrência de cada palavra
 	val wordCounts = tokenizedRDD.map( palavra => (palavra,1) ).reduceByKey( (x, y) => x + y)
-    
-    System.out.println(wordCounts.collect().mkString(", "))
+	
+	System.out.println(wordCounts.collect().mkString(", "))
   }
 }
