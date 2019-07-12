ec4b57df1332fd4c6cf9b342fe6b13dccf908f41
Eduardo Miranda <eduardo.dpm@gmail.com>
2019-07-11 21:04:08 -0300

    Simplificação do código e ajuste do POM para novas versões: Spark 2.4.1 e Scala 2.11.12

--- codigo/spark/WordCount/src/main/scala/com/fundamentosbigdata/SparkWordCount.scala
+++ codigo/spark/WordCount/src/main/scala/com/fundamentosbigdata/SparkWordCount.scala
@@ -4,15 +4,14 @@ import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.SparkContext._
 
 object SparkWordCount {
+
   def main(args: Array[String]) {
     val context = new SparkContext(new SparkConf().setAppName("Spark Word Count"))
     val input = context.textFile(args(0))
 
-    val tokens = input.flatMap(line => line.split(" "))
-    val wordCounts = tokens.
-      map(word => (word, 1)).
-      reduceByKey((x, y) => x + y)
+    val tokens = input.flatMap(line => line.split(" ")).map(word => (word, 1))
+                 .reduceByKey(_ + _)
 
-    System.out.println(wordCounts.collect().mkString(", "))
+    System.out.println(tokens.collect().mkString(", "))
   }
 }
